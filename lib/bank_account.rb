584bdb5ef8ce8c7afa50ca784635760be793a2da
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-04 18:28:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
 
-attr_accessor :user , :balance
-
-def initialize (user, balance)
+attr_accessor :balance , :status
+attr_reader :name
+def initialize (name, balance = 1000 , status = "open")
   @user = user
   @balance = balance
+  @status = status
 end
 
 end
