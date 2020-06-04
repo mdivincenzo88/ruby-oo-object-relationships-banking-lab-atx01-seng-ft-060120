54655a82ff56d694c8985857fe74f2000e94d988
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-04 18:44:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,8 @@
 class Transfer
   # your code here
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
 end
